6cae4f00d55a64e61b704e7bf9ddf5120e483bea
Aarti Jaripatke <aartijaripatke@Aartis-MacBook-Air-2.local>
2019-12-10 02:44:12 +0530

    Second Branch via command line

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world!");
 		System.out.println("FirstBranch");
+		System.out.println("Second Branch via command line");
 	}
 
 }
